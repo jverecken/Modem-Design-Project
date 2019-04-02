97175fceb1c3d780a8c4aa7c58f5606a7390a7aa
jverecken <45732366+jverecken@users.noreply.github.com>
2019-04-02 15:59:33 +0200

    begin step 2

--- project_step1.m
+++ project_step1.m
@@ -1,3 +1,4 @@
+%% code for step 1
 close all;
 clear all;
 
@@ -5,8 +6,8 @@ clear all;
 N = 128; % number of subcarriers
 fc = 2e9; % carrier frequency
 deltaf = 15e3; % subcarrier spacing
-L = 16; % prefix length
-indexK = [0 N/2-1 N/2 N/2+1]; % position of null tones
+L = 32; % prefix length
+indexK = []; %[0 N/2 + (-5:5) ]; % position of null tones
 K = length(indexK); % number of null tones
 
 %% generate symbols
@@ -14,10 +15,11 @@ N_symbols = (N-K)*1e3;
 tx_symbols = floor(4*rand(1,N_symbols));
 
 %4QAM modulation
-s = qammod(tx_symbols,4)/sqrt(2); 
+s = qammodBis(tx_symbols,2,2); 
 
 %% ofdm modulation
 x = ofdmmod(s,N,L,K,indexK);
+x = x/(x*x'/N_symbols);
 
 % pulse shaping
 M = 10; % oversampling factor
@@ -48,7 +50,7 @@ SER = zeros(1,N_SNR);
 for i = 1:N_SNR
     %% generate noise
     noise = (randn(1,length(x)) + 1i*randn(1,length(x)))/sqrt(2);
-    nu = ((N-K)/(N+L))*(Es_N0(i))^-1*noise;
+    nu = N/(N+L)*sqrt((Es_N0(i))^-1)*noise; %((N-K)/(N+L))
     
     % received signal
     r = r_nf + nu;
@@ -56,7 +58,7 @@ for i = 1:N_SNR
     %% ofdm demodulation
     s_estimated = ofdmdemod(r,N,L,K,indexK);
     
-    rx_symbols = qamdemod(s_estimated*sqrt(2),4); %4QAM demod
+    rx_symbols = qamdemodBis(s_estimated,2,2); %4QAM demod
     SER(i) = sum(rx_symbols ~= tx_symbols)/N_symbols;
     
     % constellation plot
