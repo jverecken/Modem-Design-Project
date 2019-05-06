01e60b7fe6579ea7e3ec1e126f36c486d8406bdb
jverecken <45732366+jverecken@users.noreply.github.com>
2019-05-06 17:28:31 +0200

    MAP
    map added

--- project_step3.m
+++ project_step3.m
@@ -6,11 +6,14 @@ clearvars;
 N = 128; % number of subcarriers
 L = 8; % length of the channel coefficients
 
-M = 100; % number of trials
-SNR = linspace(0,20,11); % db
+M = 1000; % number of trials
+SNR = linspace(0,10,21); % db
 Es_N0 = 10.^(SNR/10); % noise power
 N_SNR = length(Es_N0);
-mse = zeros(N_SNR,M);
+mseML = zeros(N_SNR,M);
+mseMAP = zeros(N_SNR,M);
+
+sigma2 = 1/(2*L);
 
 
 for N_blocks=1:6 % number of blocks of training sequence
@@ -19,8 +22,7 @@ for N_blocks=1:6 % number of blocks of training sequence
     for i=1:N_SNR
         for j=1:M
             % channel
-            hTrue = 1/sqrt(2)*(randn(L,1)+1i*randn(L,1));
-            hTrue = hTrue/norm(hTrue);
+            hTrue = sqrt(sigma2)*(randn(L,1)+1i*randn(L,1));
             lambda = repmat(fft(hTrue,N),N_blocks,1);
             
             % noise
@@ -30,18 +32,26 @@ for N_blocks=1:6 % number of blocks of training sequence
             r = lambda.*I+noise;
             
             % ML estimator
-            A = (I.*W(:,1:L));
-            hEst = A\r;
+            A1 = (I.*W(:,1:L));
+            hEstML = A1\r; % automatically solve with LS
             
-            mse(i,j) = (hTrue-hEst(1:L))'*(hTrue-hEst(1:L));
+            % MAP estimator with knowledge of noise power and h distr
+            A2 = [A1;sqrt((Es_N0(i)^-1)/(sigma2))*eye(L)];
+            hEstMAP = A2\[r;zeros(L,1)];
+            
+            mseML(i,j) = (hTrue-hEstML(1:L))'*(hTrue-hEstML(1:L));
+            mseMAP(i,j) = (hTrue-hEstMAP(1:L))'*(hTrue-hEstMAP(1:L));
         end
     end
-    mse = mean(mse,2);
+    mseML = mean(mseML,2);
+    mseMAP = mean(mseMAP,2);
     
-    semilogy(SNR,mse,'.-','linewidth',1.2,'markersize',15); hold on;
+    semilogy(SNR,mseML,'.-','linewidth',1.2,'markersize',15); hold on;
+    semilogy(SNR,mseMAP,'.--','linewidth',1.2,'markersize',15); hold on;
     xlabel('SNR [dB]');
     ylabel('MSE');
-    label{N_blocks}=sprintf('%d blocks',N_blocks);
+    label{2*N_blocks-1}=sprintf('%d blocks ML',N_blocks);
+    label{2*N_blocks}=sprintf('%d blocks MAP',N_blocks);
 end
 title(sprintf('%d-taps normalised Rayleigh channel\naveraged on %d trials',L,M));
 legend(label);
